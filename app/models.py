0957f5d2270634ef4477c04630bf6d9e4c2e2dc7
sanachola <sam.achola@live.com>
2017-11-29 13:14:24 +0300

    UPDATE: Restaurants Model

--- app/models.py
+++ app/models.py
@@ -59,3 +59,37 @@ class Location(db.Model):
     def __repr__(self):
         return "<Location: {}>".format(self.location)
 
+class Restaurant(db.Model):
+    """This class represents the Restaurant Table."""
+
+    __tablename__ = 'restaurants'
+
+    id = db.Column(db.Integer, primary_key=True)
+    restaurant_name = db.Column(db.String(80))
+    restaurant_email = db.Column(db.String(80))
+    restaurant_county = db.Column(db.String(80))
+    restaurant_location = db.Column(db.String(80))
+    restaurant_minorder = db.Column(db.String(80))
+    restaurant_phone = db.Column(db.String(80))
+    restaurant_mobile = db.Column(db.String(80))
+    restaurant_about = db.Column(db.String)
+    restaurant_delivery = db.Column(db.String(80))
+    
+    def __init__(self, restaurant_name, restaurant_email, restaurant_county, restaurant_location, restaurant_minorder, restaurant_phone, restaurant_mobile, restaurant_about, restaurant_delivery):
+        """Initialize the restaurant model."""
+        self.restaurant_name = restaurant_name
+        self.restaurant_email = restaurant_email
+        self.restaurant_county = restaurant_county
+        self.restaurant_location = restaurant_location
+        self.restaurant_minorder = restaurant_minorder
+        self.restaurant_phone = restaurant_phone
+        self.restaurant_mobile = restaurant_mobile
+        self.restaurant_about = restaurant_about
+        self.restaurant_delivery = restaurant_delivery
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def __repr__(self):
+        return "<Restaurant: {}>".format(self.restaurant_name)
